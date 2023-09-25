2c8cd3fde795189c0898f339cf6f0c2d0ef5276c
Abiel Borrego Pacheco <abborreg@c2r15s3.42madrid.com>
2023-09-25 21:17:07 +0200

    strnstr y memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,33 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abborreg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/20 20:32:58 by abborreg          #+#    #+#             */
-/*   Updated: 2023/09/25 21:15:11 by abborreg         ###   ########.fr       */
+/*   Created: 2023/09/23 12:47:23 by abborreg          #+#    #+#             */
+/*   Updated: 2023/09/23 14:22:17 by abborreg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *big, const char *tiny, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t	i;
-	size_t	i2;
+	size_t 	i;
 
-	if (!*tiny)
-		return ((char *)big);
-	while (big[i2])
+	if (!b)
+		return (NULL);
+	i = 0;
+	while (i < len)
 	{
-		i = 0;
-		while (big[i2 + i] == tiny[i])
-		{
-			if (!tiny[++i])
-				return ((char *)(big + i2));
-		}
-		i2++;
+		*(unsigned char*)(b + i) = (unsigned char)c;
+		i++;
 	}
-	return (0);
+	return (b);
 }
